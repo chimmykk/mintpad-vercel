d826b06a6f3bb2e8614df674fb61cd6e66b50b17
sem22-dev <thotjj22@gmail.com>
2023-11-01 18:18:25 +0530

    landingpage

--- components/sections/description2.tsx
+++ components/sections/description2.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Description2() {
     return(
         <main className=" flex flex-col gap-32 bg-bgDark px-4 xl:px-64 py-16">
-            <div className="flex justify-between gap-4 ">
+            <div className="flex flex-col md:flex-row justify-between gap-4 ">
                 <h1 className="text-[20px]  h-fit">
                     Join other creators and teams who’ve used Mintpad to launch and manage their NFT projects.
                 </h1>
