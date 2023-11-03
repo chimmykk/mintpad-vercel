5570ea3d34abb00777985786af00d096a24abaa3
sem22-dev <thotjj22@gmail.com>
2023-11-03 16:14:59 +0530

    blurDataURL=data:image/jpeg... placeholder=blur priority={true}

--- components/mintpad/button.tsx
+++ components/mintpad/button.tsx
@@ -2,7 +2,7 @@
 import { ReactNode } from 'react';
 
 interface ButtonProps {
-  loading: boolean | string;
+  loading: boolean | string ;
   children: ReactNode;
 }
 
@@ -10,7 +10,7 @@ export default function Button({ loading, children }: ButtonProps) {
   return (
     <button
       type="submit"
-      disabled={loading}
+      // disabled={loading}
       className="inline-block font-medium relative px-3 xl:px-10 py-2.5 text-xs bg-primary-600 border border-transparent rounded-md text-white text-center hover:bg-primary-700 active:bg-primary-700 focus:outline-none focus:border-primary-700 disabled:opacity-25 transition ease-in-out duration-150"
     >
       {loading && (
